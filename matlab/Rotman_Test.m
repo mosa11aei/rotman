98fc55b884888ee25a30c90246cfef1505d8e3c7
Ali Mosallaei <alimos@umich.edu>
2025-05-14 19:19:17 -0400

    nice, works!

--- matlab/Rotman_Test.m
+++ matlab/Rotman_Test.m
@@ -3,7 +3,7 @@ clear
 clf
 
 v0 = 299792458;
-f=10e9;
+f=15e9;
 lambda0 = v0/10e9;
 lambda = v0/f;
 
@@ -17,4 +17,27 @@ y3 = (1:1:Na)*d - (Na + 1)*d/2;
 theta = linspace(-2*pi*theta/360, 2*pi*theta/360, Nb);
 dPhase = (2*pi/lambda).*(y3.')*sin(theta);
 
-data = sparameters('../hfss/Rotman_solo_SParams.s9p');
+data = sparameters('Rotman_solo_SParams.s17p');
+
+Freq = data.Frequencies;
+SnP = data.Parameters;
+freqrange = 1:length(Freq);
+
+C = {'c', 'm', 'b', 'r', 'k', 'r', 'b', 'm', 'c'};
+
+for ind = 1:9
+    freq_ind = freqrange(Freq == f);
+    plot(unwrap(angle(SnP(10:17,ind,freq_ind))) - angle(SnP(10,ind,freq_ind)), 'color', C{ind}, 'LineWidth', 3)
+
+    hold on
+    plot(unwrap(dPhase(:,ind)) - dPhase(1,ind), '--', 'color', C{ind}, 'LineWidth', 3)
+end
+
+ylabel('Beam-forming unwrapped phase (rad)')
+xlabel('Array element number')
+hold on
+grid on
+
+qw{1} = plot(nan, 'k-', 'LineWidth', 3);
+qw{2} = plot(nan, 'k--', 'LineWidth', 3);
+legend([qw{:}], {'HFSS', 'Analytical (GO)'}, 'location', 'northwest');
